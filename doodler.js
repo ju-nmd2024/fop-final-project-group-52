bef65a2a50bdc76016998c7f0ee1d4c02d7b8b56
Maxliljeqvist <lima24nb@student.ju.se>
2025-02-14 02:18:56 +0100

    quality of life
    just reducing gravity

--- doodler.js
+++ doodler.js
@@ -5,7 +5,7 @@ export default class doodler {
     this.height = 40;
     this.width = 40;
     this.velocity = 0;
-    this.gravity = 0.3;
+    this.gravity = 0.2,8;
     this.jumpForce = 8;
   }
 
